748872ff4f44f8f1d8a809349a6b444491f53e56
ipickering (lab) <ign.pickering@gmail.com>
2019-05-20 15:25:40 -0400

    Fixes training_nnp formatting

--- examples/nnp_training.py
+++ examples/nnp_training.py
@@ -45,21 +45,48 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 Rcr = 5.2000e+00
 Rca = 3.5000e+00
 EtaR = torch.tensor([1.6000000e+01], device=device)
-ShfR = torch.tensor([9.0000000e-01, 1.1687500e+00, 1.4375000e+00, 1.7062500e+00, 1.9750000e+00, 2.2437500e+00, 2.5125000e+00, 2.7812500e+00, 3.0500000e+00, 3.3187500e+00, 3.5875000e+00, 3.8562500e+00, 4.1250000e+00, 4.3937500e+00, 4.6625000e+00, 4.9312500e+00], device=device)
+ShfR = torch.tensor([9.0000000e-01, 
+                    1.1687500e+00, 
+                    1.4375000e+00, 
+                    1.7062500e+00, 
+                    1.9750000e+00, 
+                    2.2437500e+00, 
+                    2.5125000e+00, 
+                    2.7812500e+00, 
+                    3.0500000e+00, 
+                    3.3187500e+00, 
+                    3.5875000e+00, 
+                    3.8562500e+00, 
+                    4.1250000e+00, 
+                    4.3937500e+00, 
+                    4.6625000e+00, 
+                    4.9312500e+00], 
+                    device=device)
 Zeta = torch.tensor([3.2000000e+01], device=device)
-ShfZ = torch.tensor([1.9634954e-01, 5.8904862e-01, 9.8174770e-01, 1.3744468e+00, 1.7671459e+00, 2.1598449e+00, 2.5525440e+00, 2.9452431e+00], device=device)
+ShfZ = torch.tensor([1.9634954e-01, 
+                    5.8904862e-01, 
+                    9.8174770e-01, 
+                    1.3744468e+00, 
+                    1.7671459e+00, 
+                    2.1598449e+00, 
+                    2.5525440e+00, 
+                    2.9452431e+00], 
+                    device=device)
 EtaA = torch.tensor([8.0000000e+00], device=device)
-ShfA = torch.tensor([9.0000000e-01, 1.5500000e+00, 2.2000000e+00, 2.8500000e+00], device=device)
+ShfA = torch.tensor([9.0000000e-01, 
+                    1.5500000e+00, 
+                    2.2000000e+00, 
+                    2.8500000e+00], 
+                    device=device)
 num_species = 4
 aev_computer = torchani.AEVComputer(Rcr, Rca, EtaR, ShfR, EtaA, Zeta, ShfA, ShfZ, num_species)
+#order of energies is H C N O
 energy_shifter = torchani.utils.EnergyShifter([
-    -0.600952980000,  # H
-    -38.08316124000,  # C
-    -54.70775770000,  # N
-    -75.19446356000,  # O
-])
+    -0.600952980000,  
+    -38.08316124000,  
+    -54.70775770000,
+    -75.19446356000,])
 species_to_tensor = torchani.utils.ChemicalSymbolsToInts('HCNO')
-
 
 ###############################################################################
 # Now let's setup datasets. Note that here for our demo purpose, we set both
