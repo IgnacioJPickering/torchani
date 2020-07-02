1983f9348eef1d78a582491787c02652ad7f2cc0
ipickering <ign.pickering@gmail.com>
2020-07-02 00:28:42 -0400

    comment on unique_consecutive

--- torchani/onnx/onnx_workarounds.py
+++ torchani/onnx/onnx_workarounds.py
@@ -1,17 +1,20 @@
 import torch
 from torch import Tensor
+
+
 # workarounds for operations unsupported in onnx opset 11.  these functions
 # reproduce the needed behavior of aten operations by using operations in
 # opset 11 only
-
-
-
-
 def opset11_index_add():
     pass
 
 
 def opset11_unique_consecutive():
+    # unique consecutive and unique are very similar, with unique being
+    # slower since it deletes non consecutive repetititons, and has to 
+    # sort in order to do that. Since in AEVComputer stuff is already 
+    # sorted when unique_consecutive is called, we can call unique 
+    # with some extra overhead without issue
     pass
 
 
@@ -21,9 +24,17 @@ def opset11_any(x: Tensor) -> Tensor:
     return x
 
 
-def opset11_repeat_interleave(times_to_repeat: Tensor, sequence: bool = False) -> Tensor:
+def opset11_repeat_interleave(times_to_repeat: Tensor,
+                              sequence: bool = False) -> Tensor:
+    # TODO: sequence is a convenience overload that instead of repeating
+    # the numbers 0 1 2 3 as 000 111 22 3 etc
+    # grabs the sequence 0 1 2 3 and repeats 0123 123 23 3 this is basically
+    # only done so that triu_indices can be used this is a hack and confusing,
+    # and should be explained better / implemented better
+
     # only single argument overload is needed for AEVComputer
-    assert len(times_to_repeat.shape) == 1, 'repeat_interleave_single_argument only accepts 1D tensors'
+    assert len(times_to_repeat.shape
+               ) == 1, 'opset11_repeat_interleave only accepts 1D tensors'
 
     max_times_to_repeat = times_to_repeat.max()
     numbers_to_repeat = torch.arange(len(times_to_repeat),
@@ -31,23 +42,21 @@ def opset11_repeat_interleave(times_to_repeat: Tensor, sequence: bool = False) -
                                      dtype=torch.long)
     # First repeat numbers_to_repeat by replicating each number to the maximum
     # value later select the ones wanted according to a mask
-    numbers_to_repeat = numbers_to_repeat.repeat(max_times_to_repeat,
-                                                 1)
-    if not sequence:  
-        # this is a convenience overload to be able to calculate triu_indices
-        numbers_to_repeat = numbers_to_repeat.t()
-
-    numbers_to_repeat = numbers_to_repeat.flatten()
+    numbers_to_repeat = numbers_to_repeat.repeat(max_times_to_repeat, 1)
 
     # Now build a mask to select only the number of repetitions necessary for
     # each row
     if sequence:
         # this is a convenience overload to be able to calculate triu_indices
-        repeats_until_maximum = torch.arange(max_times_to_repeat,0, -1, 
-                                         device=times_to_repeat.device,
-                                         dtype=torch.long)
+        numbers_to_repeat = numbers_to_repeat.flatten()
+        repeats_until_maximum = torch.arange(max_times_to_repeat,
+                                             0,
+                                             -1,
+                                             device=times_to_repeat.device,
+                                             dtype=torch.long)
         mask = (repeats_until_maximum <= times_to_repeat.view(-1, 1))
     else:
+        numbers_to_repeat = numbers_to_repeat.t().flatten()
         repeats_until_maximum = torch.arange(max_times_to_repeat,
                                              device=times_to_repeat.device,
                                              dtype=torch.long)
@@ -58,9 +67,9 @@ def opset11_repeat_interleave(times_to_repeat: Tensor, sequence: bool = False) -
 
 
 def opset11_triu_indices(size):
-    # triu indices using a fixed offset of 1, and with a fixed 
+    # triu indices using a fixed offset of 1, and with a fixed
     # row / cols of size x size
-    times_to_repeat_upper = torch.arange(size - 1, 0, -1 )
+    times_to_repeat_upper = torch.arange(size - 1, 0, -1)
     upper = opset11_repeat_interleave(times_to_repeat_upper)
     lower = opset11_repeat_interleave(times_to_repeat_upper, sequence=True) + 1
     indices = torch.cat((upper.unsqueeze(0), lower.unsqueeze(0)), dim=0)
@@ -79,4 +88,3 @@ class Opset11CELU(torch.nn.Module):
 
     def extra_repr(self) -> str:
         return 'alpha={}'.format(self.alpha)
-
