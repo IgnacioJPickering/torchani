14c494eaf2cd46461b8440a10083ca49e0b3fd0a
ipickering <ign.pickering@gmail.com>
2020-09-19 00:46:58 -0400

    Change 0th index of periodic table

--- torchani/utils.py
+++ torchani/utils.py
@@ -410,8 +410,10 @@ def get_atomic_masses(species):
     masses = default_atomic_masses[species]
     return masses
 
-
-PERIODIC_TABLE = """
+# This constant, when indexed with the corresponding atomic number, gives the
+# element associated with it. Note that there is no element with atomic number
+# 0, so None is returned in this case.
+PERIODIC_TABLE = [None] + """
     H                                                                                                                           He
     Li  Be                                                                                                  B   C   N   O   F   Ne
     Na  Mg                                                                                                  Al  Si  P   S   Cl  Ar
