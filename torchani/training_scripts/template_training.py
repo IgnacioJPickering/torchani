a83ded2238722c6593040bee3e6901c0029f7742
ipickering <ign.pickering@gmail.com>
2020-09-22 22:44:11 -0400

    load from yaml in the template

--- torchani/training_scripts/template_training.py
+++ torchani/training_scripts/template_training.py
@@ -1,26 +1,36 @@
 from pathlib import Path
-
+ 
 import torch
+import yaml
 import torch.utils.tensorboard
 from tqdm import tqdm
 
+import torchani
 from torchani import data
 from torchani.modules import TemplateModel
-from torchani.training import RootAtomsLoss, validate_energies, init_traditional
+from torchani.training import validate_energies, init_traditional
+
+from torch import optim
+from torch.optim import lr_scheduler
 
 # get device to run the training
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+# Get the configuration for the training
+yaml_path = '../training/hyper.yaml'
+with open(yaml_path, 'r') as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+
 # setup model and initialize parameters
 # this uses H C N O species and by default has periodic_table_index on
 # setting shift before output to false makes the model NOT add saes before output
-model = TemplateModel.like_ani1x().to(device).shift_before_output_(False)
+model = TemplateModel.from_yaml(config).to(device).shift_before_output_(False)
 model.apply(init_traditional)
 
-# Some configuration variables
-batch_size = 2560
-max_epochs = 100
-early_stopping_learning_rate = 1.0e-5
+# Some configuration variables, which are general
+batch_size = config['batch_size'] or 2560 
+max_epochs = config['max_epochs'] or 100
+early_stopping_lr = config['early_stopping_lr'] or 1.0e-5
 
 # setup training and validation sets
 data_path = Path(__file__).resolve().parent.joinpath('../../dataset/ani1-up_to_gdb4/ani_gdb_s01.h5').resolve()
@@ -30,10 +40,10 @@ training = training.collate(batch_size).cache()
 validation = validation.collate(batch_size).cache()
 print('\n')
 
-# setup optimizer, scheduler, loss
-optimizer = torch.optim.AdamW(model.parameters(), lr=0.001, betas=(0.9, 0.999), weight_decay=0.00001, amsgrad=False, eps=1e-8)
-lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=50, threshold=0)
-loss_function = RootAtomsLoss()
+# setup optimizer, scheduler and loss
+optimizer = getattr(optim, config['optimizer']['class'])(model.parameters(), **config['optimizer']['kwargs'])
+lr_scheduler = getattr(lr_scheduler, config['lr_scheduler']['class'])(optimizer, **config['lr_scheduler']['kwargs'])
+loss_function = getattr(torchani.training, config['loss']['class'])()
 
 # Tensorboard logging
 tensorboard_path = Path(__file__).resolve().parent
@@ -66,12 +76,16 @@ if  lr_scheduler.last_epoch == 0:
     tensorboard.add_scalar('learning_rate', optimizer.param_groups[0]['lr'], lr_scheduler.last_epoch)
 
 total_conformations = len(training) 
-print("\nTraining starting from epoch", lr_scheduler.last_epoch + 1)
+print("Training starting from epoch", lr_scheduler.last_epoch + 1)
 
 for _ in range(lr_scheduler.last_epoch, max_epochs):
 
     # Training loop per epoch
-    for i, conformation in tqdm(enumerate(training), total=total_conformations, desc=f'epoch {lr_scheduler.last_epoch}'):
+    if config['use_tqdm']: 
+        conformations = tqdm(enumerate(training), total=total_conformations, desc=f'epoch {lr_scheduler.last_epoch}')
+    else:
+        conformations = enumerate(training)
+    for i, conformation in conformations:
         species = conformation['species'].to(device)
         coordinates = conformation['coordinates'].to(device).float()
         true_energies = conformation['energies'].to(device).float()
@@ -103,7 +117,7 @@ for _ in range(lr_scheduler.last_epoch, max_epochs):
     tensorboard.add_scalar('learning_rate', optimizer.param_groups[0]['lr'] , lr_scheduler.last_epoch)
     
     # Early stopping
-    if optimizer.param_groups[0]['lr'] < early_stopping_learning_rate:
+    if optimizer.param_groups[0]['lr'] < early_stopping_lr:
         break
 
     # Checkpoint
