7731f0cd4a2e2bbccdf71ca55812e3aee43a1816
ipickering <ign.pickering@gmail.com>
2020-09-28 18:16:23 -0400

    add sharp gaussian init"

--- torchani/training/training.py
+++ torchani/training/training.py
@@ -34,6 +34,18 @@ def init_traditional(m):
         if m.bias is not None:
             torch.nn.init.zeros_(m.bias)
 
+def init_sharp_gaussian(m, scale=0.001):
+    if isinstance(m, torch.nn.Linear):
+        # if this is an output linear layer use a smaller scale
+        # for init
+        if m.weight.shape[0] == 1:
+            torch.nn.init.normal_(m.weight, mean=0.0, std=scale)
+        else:
+            torch.nn.init.kaiming_normal_(m.weight, a=1.0)
+
+        if m.bias is not None:
+            torch.nn.init.zeros_(m.bias)
+
 def reproducible_init_nobias(m):
     if isinstance(m, torch.nn.Linear):
         torch.nn.init.constant_(m.weight, 1.)
