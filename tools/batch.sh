be28bada93b28291dd5dff9101c8f426ad652bd6
ipickering <ign.pickering@gmail.com>
2020-10-13 18:55:41 -0400

    delete batch

--- tools/batch.sh
+++
@@ -1,7 +0,0 @@
-cd /home/ignacio/Git-Repos/torchani/tools || exit 1
-python ./md_scaling_benchmark.py --model ani1x_one --file-name ani1x_one_rtx 
-python ./md_scaling_benchmark.py --model ani1x_all --file-name ani1x_all_rtx
-python ./md_scaling_benchmark.py --model ani2x_one --file-name ani2x_one_rtx
-python ./md_scaling_benchmark.py --model ani2x_all --file-name ani2x_all_rtx
-python ./md_scaling_benchmark.py --model ani1ccx_one --file-name ani1ccx_one_rtx
-python ./md_scaling_benchmark.py --model ani1ccx_all --file-name ani1ccx_all_rtx
